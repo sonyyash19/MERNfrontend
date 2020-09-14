056f8f75ba303c8c5062e66b7fb0713898691650
Yashsoni <yashsony17@gmail.com>
2020-09-14 16:28:02 +0530

    message

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,8 +1,93 @@
 import React, {useState} from 'react'
 import Base from '../core/Base'
-import {Link} from 'react-router-dom'
+import { Link, Redirect} from 'react-router-dom'
+
+import { signin, authenticate, isAuthenticated} from '../auth/helper'
 
 const Signin = () => {
+
+    const [values, setValues] = useState({
+        email: "y@yash.com",
+        password: "12345",
+        error: "",
+        loading: false,
+        didRedirect: false
+    });
+
+    const { email, password, error, loading, didRedirect } = values;
+    const {user} = isAuthenticated;
+
+    const handleChanges = name => event => {
+        setValues({
+            ...values, 
+            error: false, 
+            [name]: event.target.value});
+    };
+
+    const onSubmit = event => {
+        event.preventDefault();
+        setValues({
+            ...values,
+            error: false,
+            loading:true
+        })
+        signin({email, password})
+        .then( data => {
+            if(data.error){
+                setValues({
+                    ...values,
+                    error: data.error,
+                    loading: false
+                })
+
+            }
+            else{
+                authenticate(data, () => {
+                    setValues({
+                        ...values,
+                        didRedirect: true
+                    })
+                })
+            }
+        })
+        .catch(console.log("Signin request failed"))
+    };
+
+    const performRedirect = () => {
+        if(didRedirect){
+            if(user && user.role === 1) {
+                return <Redirect to="/admin/dashboard" />;
+            }else {
+                return <Redirect to="/user/dashboard" />;
+            }
+        }
+        if(isAuthenticated()){
+            return <Redirect to="/" />;
+        }
+    };
+
+    const loadingMessage = () => {
+        return(
+            loading && (
+                <div className="alert alert-info">
+                    <h2>loading...</h2>
+                </div>
+            )
+        );
+    };
+
+    const errorMessage = () => {
+        return(
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <div className="alert alert-danger" 
+                        style={{ display: error ? "" : "none" }}>
+                            {error}
+                    </div>
+                </div>
+         </div>
+        );
+    };
 
     const signInForm = () => {
         return (
@@ -11,13 +96,13 @@ const Signin = () => {
                     <form>
                         <div className="form-group">
                             <label className="text-light">Email</label>
-                                <input className="form-control"  type="email" />
+                                <input onChange={handleChanges("email")} value={email} className="form-control"  type="email" />
                         </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
-                                <input className="form-control" type="text" />
+                                <input onChange={handleChanges("password")} value={password} className="form-control" type="password" />
                         </div>
-                        <button className="btn btn-success btn-block">Submit</button>
+                        <button onClick={onSubmit} className="btn btn-success btn-block">Submit</button>
                     </form>
                 </div>
             </div>
@@ -26,7 +111,11 @@ const Signin = () => {
 
     return(
         <Base title="SignIn" description="A page for user to signin!">
+            {loadingMessage()}
+            {errorMessage()}
             {signInForm()}
+            {performRedirect()}
+            <p className="text-white text-center">{JSON.stringify(values)}</p>
         </Base>
     );
 }
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -51,33 +51,68 @@ const Signup = () => {
                                 <input 
                                 className="form-control" 
                                 onChange={handleChanges("name")} 
-                                type="text" />
+                                type="text" 
+                                value={name}
+                                />
                         </div>
                         <div className="form-group">
                             <label className="text-light">Email</label>
                                 <input 
                                 className="form-control"  
                                 onChange={handleChanges("email")}
-                                type="email" />
+                                type="email" 
+                                value={email}
+                                />
                             </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
                                 <input 
                                 className="form-control" 
                                 onChange={handleChanges("password")}
-                                type="password" />
+                                type="password" 
+                                value={password}
+                                />
                              </div>
                         <button onClick={onSubmit} className="btn btn-success btn-block">Submit</button>
                     </form>
                 </div>
             </div>
         );
-    }
+    };
+
+    const successMessage = () => {
+        return(
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                     <div className="alert alert-success" 
+                         style={{ display: success ? "" : "none" }}>
+                             New account was created successfully.
+                             <Link to="/signin"> Login here</Link>
+                     </div>
+                 </div>
+            </div>
+        );
+    };
+
+    const errorMessage = () => {
+        return(
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <div className="alert alert-danger" 
+                        style={{ display: error ? "" : "none" }}>
+                            {error}
+                    </div>
+                </div>
+         </div>
+        );
+    };
 
     return(
         <Base title="SignUp" description="A page for user to signup!">
-            <p className="text-white text-center">{JSON.stringify(values)} </p>
+            {successMessage()}
+            {errorMessage()}
             {signUpForm()}
+            <p className="text-white text-center">{JSON.stringify(values)} </p>
         </Base>
     );
 }
